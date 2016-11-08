463dcf3e75aab6f466337b0d467519f5751e31c9
Ayush Rastogi <ayush_kp@yahoo.com>
2016-11-08 18:34:34 +0530

    API hosted on bluemix

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,4 +1,4 @@
 angular.module('starter').constant('URL', {
-    //url: "http://169.46.145.143:8080/zeus/"
-    url: "http://localhost:8080/zeus/"
+    url: "http://169.46.145.143:8080/zeus/"
+    //url: "http://localhost:8080/zeus/"
 });
